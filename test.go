3653fbaa1be4f5745f65b1378d23de82e3bec39b
wsd1 <ws_dy@sina.com>
2015-08-22 17:07:42 +0800

    Add single row query

--- test.go
+++ test.go
@@ -49,6 +49,17 @@ func main() {
 
 	defer rows.Close()
 
+	/*
+	   db.Query()表示向数据库发送一个query，defer rows.Close()非常重要，遍历rows使用rows.Next()，
+	   把遍历到的数据存入变量使用rows.Scan(), 遍历完成后检查error。有几点需要注意：
+
+	   检查遍历是否有error
+	   结果集(rows)未关闭前，底层的连接处于繁忙状态。当遍历读到最后一条记录时，会发生一个内部EOF错误，自动
+	   调用rows.Close()，但是如果提前退出循环，rows不会关闭，连接不会回到连接池中，连接也不会关闭。所以手
+	   动关闭非常重要。rows.Close()可以多次调用，是无害操作。
+
+	*/
+
 	word := new(Wikiwordcontent)
 
 	for rows.Next() {
@@ -84,5 +95,34 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	//single row query
+
+	row := db.QueryRow("SELECT word,content,compression,encryption,created,modified,visited,readonly FROM wikiwordcontent WHERE word=?", "ExMemory")
+	err = row.Scan(&word.Word, &word.Content, &word.Compression, &word.Encryption, &word.Created, &word.Modified, &word.Visited, &word.Readonly)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("---word.Word---")
+	fmt.Println(word.Word)
+	fmt.Println("---word.Content---")
+	fmt.Println(string(word.Content))
+	fmt.Println("---word.Compression---")
+	fmt.Println(word.Compression)
+	fmt.Println("---word.Encryption---")
+	fmt.Println(word.Encryption)
+
+	fmt.Println("---word.Created---")
+	fmt.Println(time.Unix(int64(word.Created), 0).Format("2006-01-02 03:04:05 PM"))
+
+	fmt.Println("---word.Modified---")
+	fmt.Println(time.Unix(int64(word.Modified), 0).Format("2006-01-02 03:04:05 PM"))
+
+	fmt.Println("---word.Visited---")
+	fmt.Println(word.Visited)
+
+	fmt.Println("---word.Readonly---")
+	fmt.Println(word.Readonly)
 
 }
